18ddd8432bbd8e51966d909b2841ff4bc1ecd593
ozzy-00 <osmars675@hotmail.com>
2025-08-25 20:18:15 -0300

    Modificação de porta de conexã

--- config.php
+++ config.php
@@ -1,13 +1,13 @@
 <?php
-
-$host = "localhost";
+$host = "127.0.0.1";   // "localhost"
 $user = "root";
 $password = "";
 $database = "users_db";
+$port = 3307; // porta que você que modifiquei 
 
-$conn = new mysqli($host, $user, $password, $database);
+$conn = new mysqli($host, $user, $password, $database, $port);
 
 if ($conn->connect_error) {
-    die("Connection failed: ". $conn->connect_error);
+    die("Connection failed: " . $conn->connect_error);
 }
 ?>
